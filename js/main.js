ef02c3ce7dc202583e748db6c68f94caf2366c95
Rocio Poza <rocio.poza.tejeda@gmail.com>
2019-07-29 08:45:01 +0200

    create conditional function without random number

--- js/main.js
+++ js/main.js
@@ -2,15 +2,30 @@
 
 //Seleccionar los elementos con los que vamos a trabajar
 const number = document.querySelector('.number');
-const input = document.querySelector('.button');
+const prube = document.querySelector('.button');
+const text = document.querySelector('.text');
+
+//Crear Objeto
+const guessNumber = {
+    magicNumber: 50, 
+    highNumber:'',
+    lowNumber: '',
+}
 
 //Crear función
 function findNumber() {
     console.log('funciono');
+    if (number.value < 50) {
+        text.innerHTML = 'demasiado bajo';
+    } else if (number.value > 50) {
+        text.innerHTML = 'demasiado alto';
+    } else {
+        text.innerHTML = '¡HAS GANADO, CAMPEONA!'; 
+    }
 }
 
 
 
 
 //Poner el botón a escuchar el evento
-input.addEventListener('click', findNumber);
+prube.addEventListener('click', findNumber);
