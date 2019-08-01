53dcc7d078ccbdd7445f699bb84290dcbada2991
Rocio Poza <rocio.poza.tejeda@gmail.com>
2019-08-01 18:02:23 +0200

    fix issues and add other message in the text div

--- js/main.js
+++ js/main.js
@@ -4,7 +4,16 @@ const number = document.querySelector('.number');
 const test = document.querySelector('.button');
 const text = document.querySelector('.text');
 const count = document.querySelector('.count');
+const reset = document.querySelector('.reset');
 let changeNumber = 0;
+
+//number = parseInt(number);
+
+const defaultElements = {
+    defaultNumber: 0, 
+    defaultCount: 0,
+    defaultText: 'Escribe un número y dale a prueba',
+}
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
@@ -13,23 +22,36 @@ function getRandomNumber(max) {
 const myRandomNumber = getRandomNumber(100);
 
 const guessNumber = {
-    magicNumber: myRandomNumber, 
+    magicNumber: '¡HAS GANADO, CAMPEONA!', 
     highNumber:'demasiado alto',
     lowNumber: 'demasiado bajo', 
+    minNumber: 'no puedes escribir un número menor que 0',
+    maxNumber: 'no puedes escribir un número mayor que 100',
 }
 
 function findNumber(event) {
-    if (number.value < guessNumber.magicNumber) {
+    if(number.value < 0) {
+        text.innerHTML = guessNumber.minNumber;
+    }else if (number.value > 100) {
+        text.innerHTML = guessNumber.maxNumber;
+    }else if (number.value < myRandomNumber) {
         text.innerHTML = guessNumber.lowNumber;
-    } else if (number.value > guessNumber.magicNumber) {
+    } else if (number.value > myRandomNumber) {
         text.innerHTML = guessNumber.highNumber;
     } else {
-        text.innerHTML = '¡HAS GANADO, CAMPEONA!'; 
+        text.innerHTML = guessNumber.magicNumber; 
     }
-    const value = event.currentTarget.value;
-    count.innerHTML = value;
     changeNumber = changeNumber + 1;
     count.innerHTML = changeNumber;
 }
 
 test.addEventListener('click', findNumber);
+
+function resetNumbers() {
+    number.value = defaultElements.defaultNumber;
+    count.innerHTML = defaultElements.defaultCount;
+    text.innerHTML = defaultElements.defaultText;
+}
+
+reset.addEventListener('click', resetNumbers);
+
